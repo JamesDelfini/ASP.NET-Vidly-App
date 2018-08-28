40f31910dd52135aadbad2445d82358daa5ce9ad
Dennis James Matildo <dj.matildo@gmail.com>
2018-08-28 20:34:35 +0800

    Example Manipulating the Controller (Action) Parameters

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -12,13 +12,42 @@ namespace Vidly.Controllers
         // GET: Movies/Random
         public ActionResult Random()
         {
-            var movies = new Movies() { Name = "Shrek!"};
+            var movies = new Movies() { Name = "Khalid <3 Issay!"};
             return View(movies);
             // Action Results
             // return Content("Hello World");
             // return HttpNotFound();
             // return new EmptyResult();
             // return RedirectToAction("Index", "Home", new{ page = 1, sortBy="name" });
+        }
+
+        /* 
+        * Sample Action Links
+        * http://localhost:50113/movies/edit/?movieId=3
+        * 
+        */
+        public ActionResult Edit(int movieId)
+        {
+            // The default Edit ID passed variable name is id stated in the router.
+            return Content("You have passed ID: " + movieId);
+        }
+
+        /* 
+         *  Sample Action Links
+         *  http://localhost:50113/movies
+         *  http://localhost:50113/movies?pageIndex=3
+         *  http://localhost:50113/movies?pageIndex=3&sortBy=ReleaseDate
+         * 
+         */
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
     }
 }
