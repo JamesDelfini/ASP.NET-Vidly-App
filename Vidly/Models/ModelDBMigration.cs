d3b3c1af74d03f7bde3d23c80c8fca1ae6d25483
Dennis James Matildo <dj.matildo@gmail.com>
2018-09-01 13:38:07 +0800

    Added a Customer Controller and Views (Index, Show) with passing the Data to the Views.

---
+++ Vidly/Models/ModelDBMigration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class ModelDBMigration : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movies> Movies { get; set; }
+    }
+    
+}
